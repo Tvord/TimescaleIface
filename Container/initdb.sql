1adc51ad04d01e9658f47e05906448d2e1839867
tvord333 <tvord333@gmail.com>
2021-10-28 13:37:35 +0000

    db tuning

--- Container/initdb.sql
+++ Container/initdb.sql
@@ -3,4 +3,6 @@ CREATE TABLE mData (ts timestamp NOT NULL,
 			value real, 
 			tag character(63) NOT NULL, 
 			textVal character(100));
-SELECT create_hypertable('mData', 'ts');
+SELECT create_hypertable('mdata', 'ts');
+SELECT add_retention_policy('mdata', INTERVAL '3 years');
+select set_chunk_time_interval('mdata', INTERVAL '7 days');
